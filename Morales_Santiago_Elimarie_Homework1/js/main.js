696a99e796ecef8c0688a1d9c5e4b7ce565b3e9d
Elimarie <iglitzi@fullsail.edu>
2015-05-04 21:48:11 -0400

    assignment
    progress

--- Morales_Santiago_Elimarie_Homework1/js/main.js
+++ Morales_Santiago_Elimarie_Homework1/js/main.js
@@ -273,6 +273,15 @@ You must use at least 3 different methods.
 	ctx.fill();
 	
 	context.beginPath();
+	context.moveTo(170,80);
+	context.bezierCurveTo(130, 100, 130, 150, 230, 150);
+	context.bezierCurveTo(250, 180, 320, 180, 340, 150);
+	context.bezierCurveTo(420, 150, 420, 120, 390, 100);
+	context.bezierCurveTo(430, 40, 370, 30, 340, 50);
+	context.bezierCurveTo(320, 5, 250, 20, 250, 50);
+	context.bezierCurveTo(200, 5, 150, 20, 170, 80);
+	context.closePath();
+	
 	
 
 }
